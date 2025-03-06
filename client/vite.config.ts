56568677018a4aa447030890647676980eb359a6
Fantasy <freedominwork@fullmetal.anonaddy.com>
2025-03-06 02:26:40 +0000

    Fix: Eslint changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import MillionLint from "@million/lint";
 
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
@@ -11,5 +12,6 @@ export default defineConfig({
     react(),
     tailwindcss(),
     tsConfigPaths(),
+    MillionLint.vite()
   ],
 });
