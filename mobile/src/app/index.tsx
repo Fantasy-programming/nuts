1fa7b97f26a4f8cfa211741834743db9a05a75f3
Fantasy <freedominwork@fullmetal.anonaddy.com>
2025-07-02 23:35:36 +0000

    switched to rn

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.text}>Edit app to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "red"
+  }
+})
