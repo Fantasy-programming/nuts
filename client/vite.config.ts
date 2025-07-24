d07ac58f28b1d596dccb1053e421343c4d0dcbc1
Fantasy <freedominwork@fullmetal.anonaddy.com>
2025-07-24 00:30:41 +0000

    Fix(client): Add missing deps

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,11 +4,13 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from 'vite-tsconfig-paths'
+import wasm from "vite-plugin-wasm";
 
 const ReactCompilerConfig = {};
 
 export default defineConfig({
   plugins: [
+    wasm(),
     tanstackRouter({
       autoCodeSplitting: true,
     }),
